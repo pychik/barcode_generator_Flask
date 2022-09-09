58f5fd93e61421285d8a38f69aa69844e5aeeab1
pychik <gendalf.grey2014@yandex.ru>
2022-09-09 17:48:45 +0300

    comments deleted

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,6 @@ from .utils import qr_form_handler
 
 @app.route(rule='/', methods=["GET"])
 def index():
-    user = {'username': 'Любитель нагенерировать код'}
-    # flash("This is a flashed message.")
     return render_template(template_name_or_list='gen_1.html', title='Главная')
 
 
@@ -14,8 +12,6 @@ def index():
 def handle_data():
     report_type = request.form.get("Report")
     numbers = request.form.get("Serial_numbers")
-    # print(request.form["Input1"], request.form["Report"], request.form["Serial_numbers"])
     message = qr_form_handler(report_type, numbers)
     flash(Markup(message))
-    # return render_template('gen_1.html', title='Главная')
     return redirect(location='/')
